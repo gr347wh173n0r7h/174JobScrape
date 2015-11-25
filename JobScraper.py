95ac363a18d0e6c58bb62a7a08165e7f132d47a2
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2015-11-24 21:10:00 -0800

    Started to scrape indeeed

--- JobScraper.py
+++ JobScraper.py
@@ -8,17 +8,32 @@ indejob = job.replace(" ", "+")
 indeloc = loc.replace(" ", "+")
 indeloc = indeloc.replace(",", "%2C")
 
-glassJob = job.replace(" ", "-")
-glassLoc = loc.replace(" ", "-")
-glassLoc = glassLoc.replace(",", "__2C-")
-
 indeUrl = "http://www.indeed.com/jobs?q=%s&l=%s" % (indejob,indeloc)
 print(indeUrl)
+
 inde = urlopen(indeUrl)
 indeSoup = BeautifulSoup(inde)
 
+Titles = indeSoup.find_all("a", {"data-tn-element": "jobTitle"})
+Companies = indeSoup.find_all("a")
+Loc = indeSoup.find_all("span", {"class": "location"})
+Desc = indeSoup.find_all("span", {"class": "summary"})
 
-glassUrl = "http://jobs.monster.com/search/?q=%s&where=%s" % (glassJob, glassLoc)
-print(glassUrl)
-glass = urlopen(glassUrl)
-glassSoup = BeautifulSoup(glass)
+print(len(Titles))
+print(len(Companies))
+print(len(Loc))
+print(len(Desc))
+
+indeTitles = []
+for m in Titles:
+    indeTitles.append(m.get('title'))
+
+indeCompanies = []
+for m in Companies:
+    # print(m)
+    if "/cmp" in m.get('href'):
+        indeCompanies.append(m.string)
+        print(m.string)
+print(len(indeCompanies))
+# indeMatch = indeMatch + indeSoup.find_all("div", {"data-tn-component": "organicJob"})
+
