62a6c8cfd69ae1a5c5331a97eeefbb7f9ba953e4
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2015-11-24 21:57:33 -0800

    Indead grabbing all needed contends, problems with company and description

--- JobScraper.py
+++ JobScraper.py
@@ -7,7 +7,7 @@ loc = input("Location:")
 indejob = job.replace(" ", "+")
 indeloc = loc.replace(" ", "+")
 indeloc = indeloc.replace(",", "%2C")
-
+#http://www.indeed.com/jobs?q=software&l=san+jose+%2Cca
 indeUrl = "http://www.indeed.com/jobs?q=%s&l=%s" % (indejob,indeloc)
 print(indeUrl)
 
@@ -15,7 +15,7 @@ inde = urlopen(indeUrl)
 indeSoup = BeautifulSoup(inde)
 
 Titles = indeSoup.find_all("a", {"data-tn-element": "jobTitle"})
-Companies = indeSoup.find_all("a")
+Companies = indeSoup.findAll("span", {"class","company"})
 Loc = indeSoup.find_all("span", {"class": "location"})
 Desc = indeSoup.find_all("span", {"class": "summary"})
 
@@ -30,10 +30,31 @@ for m in Titles:
 
 indeCompanies = []
 for m in Companies:
+    indeCompanies.append(m.string)
+
+indeLocs = []
+for m in Loc:
+    indeLocs.append(m.string)
+
+indeDesc = []
+for m in Desc:
+    indeDesc.append(m.string)
+
+for i in range(0, len(indeTitles)):
+    print(indeTitles[i])
+    if indeCompanies[i] is not None:
+        indeCompanies[i] = indeCompanies[i].replace(" ", "")
+        indeCompanies[i] = indeCompanies[i].replace("\n", "")
+    print(indeCompanies[i])
+    print(indeLocs[i])
+    print(indeDesc[i])
+    print("\n")
+
+
     # print(m)
-    if "/cmp" in m.get('href'):
-        indeCompanies.append(m.string)
-        print(m.string)
-print(len(indeCompanies))
+#     if "/cmp" in m.get('href'):
+#         indeCompanies.append(m.string)
+#         print(m.string)
+# print(len(indeCompanies))
 # indeMatch = indeMatch + indeSoup.find_all("div", {"data-tn-component": "organicJob"})
 
