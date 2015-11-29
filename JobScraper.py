f8ca6f92a8ef5940d97814037d523f3e1eeb53ba
sajayshah <shahsajay@gmail.com>
2015-11-29 00:57:59 -0800

    Fixed "none" errors by replacing all instances of .string with .get_text()

--- JobScraper.py
+++ JobScraper.py
@@ -1,44 +1,55 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-job = input("Job:")
-loc = input("Location:")
-
-indejob = job.replace(" ", "+")
-indeloc = loc.replace(" ", "+")
-indeloc = indeloc.replace(",", "%2C")
-#http://www.indeed.com/jobs?q=software&l=san+jose+%2Cca
-indeUrl = "http://www.indeed.com/jobs?q=%s&l=%s" % (indejob,indeloc)
+# job = input("Job:")
+# loc = input("Location:")
+#
+# indejob = job.replace(" ", "+")
+# indeloc = loc.replace(" ", "+")
+# indeloc = indeloc.replace(",", "%2C")
+# http://www.indeed.com/jobs?q=software&l=san+jose+%2Cca
+# indeUrl = "http://www.indeed.com/jobs?q=%s&l=%s" % (indejob, indeloc)
+indejob = "software"
+indeloc = "arizona"
+indeUrl = "http://www.indeed.com/q-%s-l-%s-jobs.html" % (indejob, indeloc)
 print(indeUrl)
 
 inde = urlopen(indeUrl)
-indeSoup = BeautifulSoup(inde)
-
+indeSoup = BeautifulSoup(inde, "html.parser")
+# print(indeSoup.prettify())
 Titles = indeSoup.find_all("a", {"data-tn-element": "jobTitle"})
-Companies = indeSoup.findAll("span", {"class","company"})
+Companies = indeSoup.findAll("span", {"class", "company"})
+# Ratings = indeSoup.find_all("span", {"class", "rating"})
 Loc = indeSoup.find_all("span", {"class": "location"})
 Desc = indeSoup.find_all("span", {"class": "summary"})
 
+
 print(len(Titles))
 print(len(Companies))
+# print(len(Ratings))
 print(len(Loc))
 print(len(Desc))
 
+
 indeTitles = []
 for m in Titles:
-    indeTitles.append(m.get('title'))
+    indeTitles.append(m.get_text())
 
 indeCompanies = []
 for m in Companies:
-    indeCompanies.append(m.string)
+    indeCompanies.append(m.get_text())
+
+# indeRatings = []
+# for m in Ratings:
+#     indeRatings.append(m.string)
 
 indeLocs = []
 for m in Loc:
-    indeLocs.append(m.string)
+    indeLocs.append(m.get_text())
 
 indeDesc = []
 for m in Desc:
-    indeDesc.append(m.string)
+    indeDesc.append(m.get_text())
 
 for i in range(0, len(indeTitles)):
     print(indeTitles[i])
@@ -46,6 +57,7 @@ for i in range(0, len(indeTitles)):
         indeCompanies[i] = indeCompanies[i].replace(" ", "")
         indeCompanies[i] = indeCompanies[i].replace("\n", "")
     print(indeCompanies[i])
+    # print(indeRatings[i])
     print(indeLocs[i])
     print(indeDesc[i])
     print("\n")
