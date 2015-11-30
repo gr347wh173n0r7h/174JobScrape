ddc5a630ea9ffe0ab374d25e8efa5e771427ab6c
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2015-11-30 13:28:29 -0800

    Indeed and Dice Both scrapped, and content saved to parallel lists.

--- JobScraper.py
+++ JobScraper.py
@@ -4,32 +4,35 @@ from bs4 import BeautifulSoup
 job = input("Job:")
 loc = input("Location:")
 
+# QUICK TEST CODE
+# job = "Software Engineering"
+# loc = "San Jose, Ca"
+
 indejob = job.replace(" ", "+")
 indeloc = loc.replace(" ", "+")
 indeloc = indeloc.replace(",", "%2C")
-# http://www.indeed.com/jobs?q=software&l=san+jose+%2Cca
+
+dicejob = job.replace(" ", "+")
+diceloc = loc.replace(" ", "+")
+diceloc = diceloc.replace(",", "%2C")
+
 indeUrl = "http://www.indeed.com/jobs?q=%s&l=%s" % (indejob, indeloc)
-# indejob = "software"
-# indeloc = "arizona"
-# indeUrl = "http://www.indeed.com/q-%s-l-%s-jobs.html" % (indejob, indeloc)
+diceURL = "https://www.dice.com/jobs?q=%s&l=%s" % (dicejob, diceloc)
+
 print(indeUrl)
+print(diceURL)
 
 inde = urlopen(indeUrl)
 indeSoup = BeautifulSoup(inde, "html.parser")
-# print(indeSoup.prettify())
+
+dice = urlopen(diceURL)
+diceSoup = BeautifulSoup(dice, "html.parser")
+
+#INDEED -----------------------------------------------------------------
 Titles = indeSoup.find_all("a", {"data-tn-element": "jobTitle"})
 Companies = indeSoup.findAll("span", {"class", "company"})
-# Ratings = indeSoup.find_all("span", {"class", "rating"})
 Loc = indeSoup.find_all("span", {"class": "location"})
 Desc = indeSoup.find_all("span", {"class": "summary"})
-
-
-print(len(Titles))
-print(len(Companies))
-# print(len(Ratings))
-print(len(Loc))
-print(len(Desc))
-
 
 indeTitles = []
 for m in Titles:
@@ -39,9 +42,6 @@ indeCompanies = []
 for m in Companies:
     indeCompanies.append(m.get_text())
 
-# indeRatings = []
-# for m in Ratings:
-#     indeRatings.append(m.string)
 
 indeLocs = []
 for m in Loc:
@@ -52,21 +52,41 @@ for m in Desc:
     indeDesc.append(m.get_text())
 
 for i in range(0, len(indeTitles)):
-    print(indeTitles[i])
     if indeCompanies[i] is not None:
         indeCompanies[i] = indeCompanies[i].replace(" ", "")
         indeCompanies[i] = indeCompanies[i].replace("\n", "")
-    print(indeCompanies[i])
-    # print(indeRatings[i])
-    print(indeLocs[i])
-    print(indeDesc[i])
-    print("\n")
+    if indeDesc[i] is not None:
+        indeDesc[i] = indeDesc[i].replace("\n", "")
+
+#DICE -------------------------------------------------------------------
+diceJobs = diceSoup.findAll('div',{'class':'serp-result-content'})
+
+diceTitles = []
+diceCompanies = []
+diceLocs = []
+diceDesc = []
+for job in diceJobs:
+    diceTitles.append(job.find("a", {"class": "dice-btn-link"}).get("title"))
+    diceCompanies.append(job.find("li", {"class", "employer"}).get_text())
+    diceLocs.append(job.find("li", {"class": "location"}).get_text())
+    diceDesc.append(job.find("div", {"class": "shortdesc"}).string)
 
 
-    # print(m)
-#     if "/cmp" in m.get('href'):
-#         indeCompanies.append(m.string)
-#         print(m.string)
-# print(len(indeCompanies))
-# indeMatch = indeMatch + indeSoup.find_all("div", {"data-tn-component": "organicJob"})
+for i in range(0, 30):
+    diceTitles[i] = diceTitles[i].replace("\n", "")
+    diceCompanies[i] = diceCompanies[i].replace("\n", "")
+    diceLocs[i] = diceLocs[i].replace("\n", "")
+    diceDesc[i] = diceDesc[i].replace("\n", "").lstrip()
 
+
+# INDEED
+# indeTitles[i]
+# indeCompanies[i]
+# indeLocs[i]
+# indeDesc[i]
+
+# DICE
+# diceTitles[i]
+# diceCompanies[i]
+# diceLocs[i]
+# diceDesc[i]
